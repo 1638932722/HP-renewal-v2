2305e933578a8b542e06fdf9d41bff96995eb5d6
Gikouzen <wg1638932722@gmail.com>
2024-07-10 11:20:40 +0900

    anima

--- js/animation.js
+++ js/animation.js
@@ -31,6 +31,24 @@ document.addEventListener("DOMContentLoaded", function() {
         stagger: 0.03
     });
 
+    // IntersectionObserver回调
+    function handleTextAnimation(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                splitTimeline.play(); // 进入视口时播放动画
+                observer.unobserve(entry.target); // 动画触发后停止观察
+            }
+        });
+    }
+
+    // 50%可见时触发
+    const textAnimationObserver = new IntersectionObserver(handleTextAnimation, {
+        threshold: 0.5
+    });
+
+    // 观察当前 textanimation 元素
+    textAnimationObserver.observe(element);
+});
 
 
   // let textAnimationElements = document.querySelectorAll('.textanimation');
@@ -44,24 +62,24 @@ document.addEventListener("DOMContentLoaded", function() {
   //         stagger: 0.03
   //     });
 
-      // IntersectionObserver回调
-      function handleTextAnimation(entries, observer) {
-          entries.forEach(entry => {
-              if (entry.isIntersecting) {
-                  splitTimeline.play(); // 进入视口时播放动画
-                  observer.unobserve(entry.target); // 动画触发后停止观察
-              }
-          });
-      }
+  //     // IntersectionObserver回调
+  //     function handleTextAnimation(entries, observer) {
+  //         entries.forEach(entry => {
+  //             if (entry.isIntersecting) {
+  //                 splitTimeline.play(); // 进入视口时播放动画
+  //                 observer.unobserve(entry.target); // 动画触发后停止观察
+  //             }
+  //         });
+  //     }
 
-      // 50%可见时触发
-      let textAnimationObserver = new IntersectionObserver(handleTextAnimation, {
-          threshold: 0.5 
-      });
+  //     // 50%可见时触发
+  //     let textAnimationObserver = new IntersectionObserver(handleTextAnimation, {
+  //         threshold: 0.5 
+  //     });
 
-      // 观察当前 textanimation 元素
-      textAnimationObserver.observe(element);
-  });
+  //     // 观察当前 textanimation 元素
+  //     textAnimationObserver.observe(element);
+  // });
     
   // アニメーション　FadeInUp
     ScrollReveal().reveal('.timeline-item', { 
